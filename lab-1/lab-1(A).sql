82abd7455937c062485972f4bcfec29a74c32a7c
VladislavSH <shishkov2004@bk.ru>
2023-10-20 19:56:25 +0400

    LR1

--- lab-1/lab-1(A).sql
+++
@@ -1,3 +0,0 @@
-use world;
-SELECT Name from city;
-SELECT Name from city where name like"A%"
---
+++ lab-1/lab-1.sql
@@ -0,0 +1,5 @@
+-- Active: 1696935710304@@127.0.0.1@3306@world
+USE world;
+SELECT Name FROM city;
+SELECT Name FROM city where name like"C%";
+SELECT Name FROM city where name like"A%";
