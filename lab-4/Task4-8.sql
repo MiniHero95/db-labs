666027a383bdea868727e71fdee66d0c827d5d16
VladislavSH <shishkov2004@bk.ru>
2023-11-06 14:42:32 +0400

    LR4

---
+++ lab-4/Task4-8.sql
@@ -0,0 +1,8 @@
+/*откройте транзакцию, удалите пользователя с memid = 37, выведите список всех членов клуба, завершите транзакцию с отказом о всех изменениях.  
+Повторно выведите список всех членов клуба.*/
+USE cd;
+START TRANSACTION;
+DELETE FROM members WHERE memid = 37;
+SELECT * FROM members;
+ROLLBACK;
+SELECT * FROM members;
