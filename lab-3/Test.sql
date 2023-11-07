8a33e6cd01b37ef98ae16a3073dd58b061b89208
VladislavSH <shishkov2004@bk.ru>
2023-11-07 18:44:10 +0400

    LR3

--- lab-3/Test.sql
+++
@@ -1,4 +0,0 @@
-USE cd;
-SELECT CONCAT(m1.firstname, ' ', m1.surname) AS fiomember, (SELECT CONCAT(m2.firstname, ' ', m2.surname) 
-FROM members m2 WHERE m2.memid = m1.recommendedby) AS fiorecommendedby FROM members m1
-WHERE m1.memid != 0 ORDER BY fiomember;
