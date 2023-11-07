66964bb572c7d77321b33a0e88b5fb9192f0dc86
VladislavSH <shishkov2004@bk.ru>
2023-11-07 10:58:55 +0400

    LR3

--- lab-3/Task3-7.sql
+++ lab-3/Task3-7.sql
@@ -3,4 +3,5 @@
 SELECT DISTINCT concat(mem.surname,' ',mem.firstname) AS membername,
 concat(rec.surname,' ',rec.firstname) AS recname
 FROM members mem, members rec 
-WHERE mem.recommendedby=rec.memid;
+WHERE mem.recommendedby=rec.memid
+ORDER BY membername;
