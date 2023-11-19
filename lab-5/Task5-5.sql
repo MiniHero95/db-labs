f7754090c4cb24b2cbe85067dbeed00be59fdc1b
VladislavSH <shishkov2004@bk.ru>
2023-11-19 21:51:23 +0400

    LR5

---
+++ lab-5/Task5-5.sql
@@ -0,0 +1,7 @@
+/*Рассчитайте количество аренд каждого из объектов клуба за сентябрь 2012 года.*/
+USE cd;
+SELECT f.facid,f.facility,COUNT(b.slots) as number_of_rents
+FROM facilities f JOIN bookings b 
+ON b.facid=f.facid
+WHERE (b.starttime>"2012-09-01" AND b.starttime<"2012-10-01")
+GROUP BY f.facid, f.facility 
