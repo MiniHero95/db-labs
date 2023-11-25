7b9718ce14a539b6a29b7f57431206d34963928b
VladislavSH <shishkov2004@bk.ru>
2023-11-25 22:34:51 +0400

    LR5_FIXED

--- lab-5/Task5-6.sql
+++ lab-5/Task5-6.sql
@@ -1,7 +1,6 @@
 /*Cоставьте список общего количества бронирований на каждый объект в месяц в 2012 году.*/
 USE cd;
-SELECT f.facid,f.facility, MONTH(b.starttime),COUNT(b.slots) as number_of_rents
-FROM facilities f JOIN bookings b 
-ON b.facid=f.facid
-WHERE YEAR(b.starttime) = 2012
+SELECT f.facid,f.facility, MONTH(b.starttime),SUM(b.slots) as number_of_rents
+FROM facilities f 
+JOIN bookings b ON b.facid=f.facid WHERE YEAR(b.starttime) = 2012
 GROUP BY f.facid, f.facility, MONTH(b.starttime)
