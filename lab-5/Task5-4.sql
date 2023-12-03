b08feda96c5909b16c795e20fb93ff875859a000
VladislavSH <shishkov2004@bk.ru>
2023-12-03 19:06:55 +0400

    LR5-FIXED

--- lab-5/Task5-4.sql
+++ lab-5/Task5-4.sql
@@ -1,6 +1,6 @@
 /*Рассчитайте количество аренд каждого из объектов клуба.*/
 USE cd;
-/*кол-во аренд всех объектов COUNT(b.slots)*/
+/*кол-во аренд всех объектов SUM(b.slots)*/
 SELECT f.facid,f.facility,SUM(b.slots) as number_of_rents
 FROM facilities f JOIN bookings b 
 ON b.facid=f.facid
