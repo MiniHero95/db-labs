ac9d4aa584c9f64181d3a0a18fddeec6eb7d9904
VladislavSH <shishkov2004@bk.ru>
2023-12-05 00:34:48 +0400

    LR6

--- lab-6/Task6-1.sql
+++ lab-6/Task6-1.sql
@@ -1,3 +1,4 @@
 /*Выберите начальное и конечное время последних 10 аренд  объектов, упорядочив их по времени их окончания.*/
+USE cd;
 SELECT starttime AS startime, ADDTIME(starttime, SEC_TO_TIME(slots*1800)) AS endtime 
 FROM bookings ORDER BY endtime DESC LIMIT 10;
