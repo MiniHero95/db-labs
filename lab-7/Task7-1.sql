db00c2c14d1f21cfb31fce562804e222d7b3f4bd
VladislavSH <shishkov2004@bk.ru>
2023-12-22 13:07:09 +0400

    LR7

--- lab-7/Task7-1.sql
+++ lab-7/Task7-1.sql
@@ -1,11 +1,12 @@
-/*Создайте функцию, которая рассчитывает стоимость каждой аренды (для каждой записи таблицы bookings).*/
-USE cd;
-CREATE FUNCTION sum_rents(facid INT, bookid INT, slots INT)
-RETURNS INT 
-DETERMINISTIC
-BEGIN 
-   DECLARE rent_cost INT
-   SET rent_count=(SELECT IF(memid=0,membercost,guestcost)*slots FROM facilities WHERE facid=facilities.facid);
-Return rent_cost;
-SELECT sum_rents (facid INT, bookid INT, slots INT) AS rent_cost FROM bookings;
-
+-- Task-7-1: Создаем функцию для расчета стоимости аренды
+DELIMITER //
+CREATE FUNCTION CalculateRentalCost(bookID INT) RETURNS DECIMAL(10, 2) DETERMINISTIC
+BEGIN
+    DECLARE cost DECIMAL(10, 2);
+    SELECT (slots * CASE WHEN memid IS NULL THEN guestcost ELSE membercost END) INTO cost
+    FROM bookings
+    WHERE bookid = bookID;
+    RETURN cost;
+END; 
+//
+DELIMITER ;
